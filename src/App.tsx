dbbed66ef0919d1a7b8d48c4c6b53244361e7671
Iamnotadonut <shadyawad225@gmail.com>
2026-06-08 05:30:50 +0300

    Fix pipeline: separated script modules out of build compilation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,1 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export default function App() {
-  return <div></div>;
-}
+import './index.css'; 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,2 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// Zero-overhead native loader
 import './index.css';
 import './script.js';
